0885945716b7c688ef03786ba1501b01f8772e12
Andres Degano <andres.degano@gmail.com>
2024-12-30 22:42:51 +0100

    nats

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import {
+  HttpStatus,
+  Logger,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { envs } from './config';
+import {
+  MicroserviceOptions,
+  RpcException,
+  Transport,
+} from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('PaymentsService');
@@ -13,17 +23,24 @@ async function bootstrap() {
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
-      // exceptionFactory: (validationErrors: ValidationError[] = []) => {
-      //   return new RpcException({
-      //     status: HttpStatus.BAD_REQUEST,
-      //     message: validationErrors.map((error) => ({
-      //       field: error.property,
-      //       error: Object.values(error.constraints).join(', '),
-      //     })),
-      //   });
-      // },
+      exceptionFactory: (validationErrors: ValidationError[] = []) => {
+        return new RpcException({
+          status: HttpStatus.BAD_REQUEST,
+          message: validationErrors.map((error) => ({
+            field: error.property,
+            error: Object.values(error.constraints).join(', '),
+          })),
+        });
+      },
     }),
   );
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.NATS_SERVERS,
+    },
+  });
+  await app.startAllMicroservices();
   await app.listen(envs.PORT || 3000);
   logger.log(`Payments ms is running on: ${await app.getUrl()}`);
 }
